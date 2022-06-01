2606a344b72dac5361c3623569113f092749d211
ZYL <503302425@qq.com>
2022-06-01 16:15:51 +0800

    Changes

--- apps/t/apps.py
+++ apps/t/apps.py
@@ -1,7 +1,7 @@
 from django.apps import AppConfig
 
 
-class ModuleCounterConfig(AppConfig):
+class TConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'apps.t'
     verbose_name = '未决回访'
